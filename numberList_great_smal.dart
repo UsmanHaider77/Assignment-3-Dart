6012dbc3782f23ba32bd0500a0867aefd3d37013
UsmanHaider77 <musmanhaider77@gmail.com>
2024-01-24 10:47:19 -0800

    List & Map

---
+++ numberList_great_smal.dart
@@ -0,0 +1,13 @@
+
+  void main(){
+List<int> numbers = [115,10,20,35,55,70,90,110];
+    if (numbers.isNotEmpty) {
+      int greatest = numbers.reduce((current, next) => current > next ? current : next);
+      int smallest = numbers.reduce((current, next) => current < next ? current : next);
+      print("List of Numbers : $numbers");
+      print("Greatest Number : $greatest");
+      print("Smallest Number : $smallest");
+    } else {
+       print("The List is Empty");
+    }
+  }
