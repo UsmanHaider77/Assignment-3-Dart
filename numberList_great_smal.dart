d39287bbdfdaa87fdcbf9b047b76719e6888ca59
UsmanHaider77 <musmanhaider77@gmail.com>
2024-01-24 10:50:28 -0800

    List & Map

--- numberList_great_smal.dart
+++ numberList_great_smal.dart
@@ -1,6 +1,6 @@
 
   void main(){
-List<int> numbers = [115,10,20,35,55,70,90,110];
+List<int> numbers = [15,10,20,35,55,70,90,110];
     if (numbers.isNotEmpty) {
       int greatest = numbers.reduce((current, next) => current > next ? current : next);
       int smallest = numbers.reduce((current, next) => current < next ? current : next);
