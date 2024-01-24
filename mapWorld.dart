3b71d9d476ac052fc24b300bd2a157528583d415
UsmanHaider77 <musmanhaider77@gmail.com>
2024-01-24 12:27:20 -0800

    List & Map

---
+++ mapWorld.dart
@@ -0,0 +1,29 @@
+
+   void main(){
+      Map<String, Map<String, String>> world = {
+    "Pakistan": {
+      "Capital City": "Islamabad",
+      "Currency": "Rupees",
+      "Language": "Urdu",
+    },
+    "Italy": {
+      "Capital City": "Rome.",
+      "Currency": "Euro",
+      "Language": "Italian",
+    },
+    "Germany": {
+      "Capita lCity": "Berlin",
+      "Currency": "Euro",
+      "Language": "German",
+    },
+  };
+  String selectedCountry = "Pakistan";
+  if (world.containsKey(selectedCountry)) {
+    Map<String, String>? countryInfo = world[selectedCountry]!;
+    print("Capital City: ${countryInfo['Capital City']}");
+    print("Currency: ${countryInfo['Currency']}");
+    print("Language: ${countryInfo['Language']}");
+  } else {
+    print("$selectedCountry is not present in the world map.");
+  }
+}                                           
