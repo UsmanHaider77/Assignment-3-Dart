4826dc89fbfecc71a2b04c80f8698b088f39113b
UsmanHaider77 <musmanhaider77@gmail.com>
2024-01-26 22:19:49 -0800

    Maximum Value in a List

---
+++ maxiValue.dart
@@ -0,0 +1,15 @@
+
+   void main(){
+List<int> numbers = [5,10,7,25,20,57,38,19];
+    if (numbers.isEmpty) {
+      print("List is Empty");
+    }
+   int maximumValue = numbers[0];
+   for (int number in numbers) {
+    if (number>maximumValue) {
+        maximumValue = number;
+    }
+     
+   }
+       print("The Maximum Value in the List is : $maximumValue");
+   }
