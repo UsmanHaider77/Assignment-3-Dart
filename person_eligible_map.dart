4703441ea49f9012320d5cf27f76b013a308d2de
UsmanHaider77 <musmanhaider77@gmail.com>
2024-01-27 22:03:04 -0800

    List & Map

---
+++ person_eligible_map.dart
@@ -0,0 +1,10 @@
+
+  void main(){
+Map person = {"Name" : "Jhon ", "Age" : 25,"isStudent": true};
+if (person["isStudent"] && person["Age"]>18) {
+    print("Student is Eligible");
+} else {
+   print("Student is not Eligible");
+}
+
+  }
