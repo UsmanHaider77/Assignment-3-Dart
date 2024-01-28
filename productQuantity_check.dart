e73109ec23855b36277240df51502cf82db3c0ba
UsmanHaider77 <musmanhaider77@gmail.com>
2024-01-27 22:09:35 -0800

    List & Map

---
+++ productQuantity_check.dart
@@ -0,0 +1,10 @@
+
+ void main(){
+ Map productDetails = {"Name": "Mobile", "Price": 50000 , "Stock" : 50};
+ if (productDetails["Stock"]>0) {
+    print("Product In Stock");
+ } else {
+   print("Product Out OF Stock");
+ }
+
+ }
